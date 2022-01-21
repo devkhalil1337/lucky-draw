8c2109b7fa17c8f0b765280bef73c13f9f156ae5
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2022-01-21 16:56:24 +0500

    display winners component and view updated

--- src/js/Controllers/displayWinnerListController.js
+++ src/js/Controllers/displayWinnerListController.js
@@ -1,6 +1,14 @@
-angular.module('luckDrawApp').controller("displayWinnerListController", ['$scope', 'navigationService', function($scope, navigationService) {
-	function init() {
-		console.log("displayWinnerListController execyted")
+angular.module('luckDrawApp').controller("displayWinnerListController", ['$scope', 'navigationService','localStorageService', function($scope, navigationService,localStorageService) {
+	
+	$scope.winners = {
+		list:[]
 	}
+	
+	function init() {
+		console.log("displayWinnerListController execyted");
+		$scope.winners.list = localStorageService.getWinnersList();
+	
+	}
+	
 	init();
 }]);
